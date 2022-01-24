6b91928f096c3ef52e254159bcc2accc979dc558
Yanochka <zavgorodnyaya-yana@mail.ru>
2022-01-24 21:56:53 +0300

    Скорpектировала код js

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// let cities = ['moscow', 'london', 'berlin', 'porto'];
+// cities[10] = 'paris';
+// cities[-1] = 'oops';
+
+
+// function getCiti (citi, value){
+//     return getCiti()
+// }
+
+
+// console.log(getCiti(cities, 1)); // 'london'
+// console.log(getCiti(cities, 4)); // null
+// console.log(getCiti(cities, 10, 'paris')); // 'paris'
+// console.log(getCiti(cities, -1, 'oops')); // 'oops'
+
+
