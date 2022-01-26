7a7555f5c111396e26f6de367b05f5e9cd19cc81
Yanochka <zavgorodnyaya-yana@mail.ru>
2022-01-26 12:23:53 +0300

    Скорректировала код js

--- js/index.js
+++ js/index.js
@@ -1,21 +1,49 @@
-// const cities = ['moscow', 'london', 'berlin', 'porto'];
+// task 1
 
-// function get(_cities){
-
-// }
-
-
-// console.log(get(cities, 1)); // 'london'
-// console.log(get(cities, 4)); // null
-// console.log(get(cities, 10, 'paris')); // 'paris'
-// console.log(get(cities, -1, 'oops')); // 'oops'
-
-
-const numbers = [1, -4, 7, 12];
-
-function sumNumbers(numbers){
-
-
+const cities = ['moscow', 'london', 'berlin', 'porto'];
+let arr = cities
+function get(arr,index,value = "null"){
+  if (index >= arr.length || index < 0){ 
+    return value }      
+  else{ 
+    return arr[index] }
 }
+
+console.log(get(cities, 1)); // 'london'
+console.log(get(cities, 4)); // null
+console.log(get(cities, 10, 'paris')); // 'paris'
+console.log(get(cities, -1, 'oops')); // 'oops'
+
+// task 2
+
+function solution(string) {
+  let b = string.split('');
+  for (let i = 0; i < b.length; i++) {
+    if (b[i] === b[i].toUpperCase()) {
+      b.splice(i, 1, ' ' + b[i])
+    }
+  }
+    return b.join('')
+}
+
+console.log(solution('camelCasing')); // "camel Casing"
+console.log(solution('indtntiFier')); // "indtnti Fier"
+
+// task 3
+
+let arrayNumbers = [1, -4, 7, 12];
+let sumNumbers = 0
+for (let i = 0; i < arrayNumbers.length; i++){
+  if (arrayNumbers[i]>0) {
+      sumNumbers += arrayNumbers[i]
+  }
+} 
+    
+console.log(sumNumbers) // 20
+
+
+
+
+
 
 
