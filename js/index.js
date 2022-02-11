1b45ffec58417f35ba2edc960b919f7c66a8a713
Yanochka <zavgorodnyaya-yana@mail.ru>
2022-02-11 22:52:04 +0300

    Реализовала функцию с помощью методов массивов reverse(), sort(), indexOf()

---
+++ js/index.js
@@ -0,0 +1,27 @@
+// task 1
+
+const names = ['john', 'smith', 'karl']; 
+let result = names.reverse();
+console.log(result) // ['karl', 'smith', 'john']
+
+function reverseNames(names){
+    return names.sort()
+};
+
+console.log(reverseNames(names)); // ['john', 'smith', 'karl']
+
+// task 2
+
+const items = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+function isPangram(pangrams){
+    for (let i = 0; i < items.length; i++){
+    if(pangrams.indexOf(items[i]) === -1){
+        return false;
+    }}
+    
+    return true;
+}
+
+console.log(isPangram("The quick brown fox jumps over the lazy dog")); // true
+console.log(isPangram("This is not a pangram")); // false
