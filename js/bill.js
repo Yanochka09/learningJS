b972d035c738ccaaf27cc2728f1eccd63c6d4228
Yanochka <zavgorodnyaya-yana@mail.ru>
2022-02-17 22:41:32 +0300

    Скорректировала ";" в коде js

--- js/bill.js
+++ js/bill.js
@@ -7,9 +7,9 @@ export default class Bill {
   
     /** @param {string} amountStr */
     addAmount(amountStr) {
-        let num = Number(amountStr)
-        this.amounts.push(num)  
-        return this.amounts
+        let num = Number(amountStr);
+        this.amounts.push(num);
+        return this.amounts;
     } 
     getCount(){
         return this.amounts.length;
@@ -19,14 +19,14 @@ export default class Bill {
         this.amounts.forEach(function(amount){
           result += amount;  
         })
-        return result
+        return result;
     }
     getAverage(){
         let result = 0;
         this.amounts.forEach(function(amount){
           result += amount;  
         })
-        return result/this.amounts.length
+        return result/this.amounts.length;
     }
 
   }
