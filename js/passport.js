fe74af78314003dca3fb0c70e9ccc4443485984a
Yanochka <zavgorodnyaya-yana@mail.ru>
2022-02-17 09:27:58 +0300

    Скорректировала проект в файле js

--- js/passport.js
+++ js/passport.js
@@ -5,24 +5,26 @@ export default class Passport {
      * @param {string} firstName
      * @param {string} lastName
      */
-        constructor(firstName, lastName){
-        this.firstName = firstName;
-        this.lastName = lastName;
+    constructor(firstName, lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
     }
-      getFirstName(){
-          return this.firstName.toLocaleLowerCase();
-      }
-      getLastName(){
-          return this.lastName.toUpperCase();
-      }
-      getFullName(){
-          return `${this.firstName} ${this.lastName}`;
-      }
-      getInitials(){
-          return `${this.firstName[0]}.${this.lastName[0]}.`;
-      }
-      getIsValidName(){
-          
-      }
-
-  }
+    getFirstName(){
+        return this.firstName.toLocaleLowerCase();
+    }
+    getLastName(){
+        return this.lastName.toUpperCase();
+    }
+    getFullName(){
+        return `${this.firstName} ${this.lastName}`;
+    }
+    getInitials(){
+        return `${this.firstName[0]}.${this.lastName[0]}.`;
+    }
+    getIsValidName(){
+        if(/^[a-zA-Z]*$/g.test(this.firstName) === /^[a-zA-Z]*$/g.test(this.lastName)){
+        return 'Yes';
+        }
+        return 'No';
+    }
+}
